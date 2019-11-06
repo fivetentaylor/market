c32589bdf2f2a9c03bfda7e61ce07b3789d60428
Taylor Sather <fivetentaylor@gmail.com>
2019-11-05 20:34:52 -0800

    Starting with python

---
+++ market.py
@@ -0,0 +1,16 @@
+
+# order = {
+#   'account': 'owner of the account',
+#   'type': 'ask | bid',
+#   'market': string: name of market,
+#   'rate': float: price you'd like to buy/sell,
+#   'amount': float: how many units at that price
+# }
+
+# markets = {
+#   'name': {
+#       'asks': [],
+#       'bids': [],
+#   }, ...
+# }
+
