70d65394b8e2a93067a793609f7d204510741bca
Taylor Sather <fivetentaylor@gmail.com>
2019-11-05 22:51:36 -0800

    Slightly more concise

--- binsearch.py
+++ binsearch.py
@@ -1,25 +1,21 @@
+from operator import gt, lt
+
 def bisect(val, array, key=lambda x: x, reverse=False):
     '''return the index of the first item >= key(val)'''
-    if len(array) == 0:
-        return 0
+    if len(array) == 0: return 0
 
-    if reverse:
-        comp = lambda x,y: x < y
-    else:
-        comp = lambda x,y: x > y
+    comp = lt if reverse else gt
 
     l, r = 0, len(array)
 
-    while (r - l) > 1:
+    while True:
         ix = (l + r) // 2
         if comp(key(val), key(array[ix])):
+            if r - l <= 1: return r
             l = ix
         else:
+            if r - l <= 1: return l
             r = ix
-
-    ix = (l + r) // 2
-    ix += int(comp(key(val), key(array[ix])))
-    return ix
 
 def insert(val, array, key=lambda x: x, reverse=False):
     i = bisect(val, array, key, reverse)
