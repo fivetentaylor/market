46b03f29577c7c2058cae1ae1ed712f738fe1043
Taylor Sather <fivetentaylor@gmail.com>
2019-11-19 20:53:50 -0800

    Starting to format

--- market.py
+++ market.py
@@ -22,9 +22,26 @@ def place_order(order: Order):
             'bid': [],
         }
 
+    market = markets[order.market]
+
     insert(
         order,
-        markets[order.market][order.kind],
+        market[order.kind],
         key=lambda m: m.rate,
         reverse=order.kind == 'bid',
     )
+
+
+    # if no orders can be filled
+    if (
+        len(market['ask']) == 0 or
+        len(market['bid']) == 0 or
+        market['ask'][0].rate > market['bid'][0].rate)
+    ):
+        return
+
+
+
+
+
+
