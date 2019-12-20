ae6a68512b37e9366136db7c2ba9e7e7538252c4
Taylor Sather <fivetentaylor@gmail.com>
2019-12-20 11:21:53 -0800

    Switched over to dictionaires and explicit types in function calls

--- market.py
+++ market.py
@@ -1,86 +1,72 @@
+from copy import deepcopy
 import dataclasses as dcs
 from binsearch import insert
 from operator import le, ge
-from typing import Dict, List
+from typing import Dict, List, Union, Literal
 from enum import Enum, auto
 
 
-class Color(Enum):
-    RED = auto()
-    GREEN = auto()
-    BLUE = auto()
+def fill_order(market: dict, maker: bool, order: dict):
+    fill = deepcopy(order)
+    fill['maker'] = maker
+    market[order['product']]['fills'].append(fill)
 
 
-@dcs.dataclass
-class Order:
-    account: str
-    kind: str
-    product: str
-    rate: float
+def place_order(
+    market: dict,
+    account: str,
+    product: str,
+    kind: Union[Literal['ask'], Literal['bid']],
+    rate: float,
     amount: float
-
-
-@dcs.dataclass
-class Fill:
-    aggressor: str
-    account: str
-    kind: str
-    product: str
-    rate: float
-    amount: float
-
-
-@dcs.dataclass
-class Market:
-    asks: List[Order]
-    bids: List[Order]
-    fills: List[Fill]
-
-
-@dcs.dataclass
-class Account:
-    name: str
-    balances: Dict[str, float]
-
-
-def fill_order(market: dict, maker: bool, order: Order):
-    market[order.product]['fills'].append(Fill(
-        aggressor = 'maker' if maker else 'taker',
-        **dcs.asdict(order)
-    ))
-
-
-def place_order(market: dict, order: Order):
-    if order.product not in market:
-        market[order.product] = {
-            'ask': [],
-            'bid': [],
+):
+    if product not in market:
+        market[product] = {
+            'asks': [],
+            'bids': [],
             'fills': [],
         }
 
-    product = market[order.product]
+    order = {
+        'account': account,
+        'product': product,
+        'kind': kind,
+        'rate': rate,
+        'amount': amount,
+    }
 
-    book_type = 'bid' if order.kind == 'ask' else 'ask'
-    comp = le if order.kind == 'ask' else ge
+    product = market[product]
+
+    book = 'bids' if kind == 'ask' else 'asks'
+    comp = le if kind == 'ask' else ge
     while (
-        len(product[book_type]) > 0 and
-        comp(order.rate, product[book_type][0].rate)
+        len(product[book]) > 0 and
+        order['amount'] > 0 and
+        comp(rate, product[book][0]['rate'])
     ):
-        if order.amount >= product[book_type][0].amount:
-            o = product[book_type].pop(0)
-            fill_order(market, True, o)
-            fill_order(market, False, dcs.replace(order, amount=o.amount, rate=o.rate))
-            order.amount -= o.amount
-        else:
-            o = product[book_type][0]
-            fill_order(market, True, dcs.replace(o, amount=order.amount))
-            fill_order(market, False, dcs.replace(order, rate=o.rate))
-            o.amount -= order.amount
+        make = deepcopy(product[book][0])
+        take = deepcopy(order)
 
-    if order.amount > 0:
+        if take['amount'] < make['amount']:
+            # make is partially filled
+            make['amount'] = take['amount']
+            product[book][0]['amount'] -= amount
+            order['amount'] = 0
+        else:
+            # make is completely filled
+            take['amount'] = make['amount']
+            product[book].pop(0)
+            order['amount'] -= make['amount']
+
+        take['rate'] = make['rate']
+        fill_order(market, True, make)
+        fill_order(market, False, take)
+
+    if order['amount'] > 0:
+        book = '%ss' % kind
         insert(
             order,
-            product[order.kind],
-            key=lambda m: m.rate,
-            reverse=order.kind == 'bid',
+            product[book],
+            key=lambda m: m['rate'],
+            reverse=book == 'bids',
         )
--- test_market.py
+++ test_market.py
@@ -1,64 +1,69 @@
 import pudb
 import market
-import dataclasses as dcs
+from copy import deepcopy
 
 def test_place_asks():
-    o = market.Order(
-        account = '123',
-        kind = 'ask',
-        product = 'test',
-        rate = 100.0,
-        amount = 5
-    )
+    o = {
+        'account': '123',
+        'kind': 'ask',
+        'product': 'test',
+        'rate': 100.0,
+        'amount': 5,
+    }
 
     m = {}
-    market.place_order(m, o)
-    market.place_order(m, dcs.replace(o, rate=101.0))
-    market.place_order(m, dcs.replace(o, rate=99.0))
+    rates = [99.0, 100.0, 101.0]
+    for rate in rates:
+        o = deepcopy(o)
+        o['rate'] = rate
+        market.place_order(m, **o)
 
-    asks = m['test']['ask']
-    assert(asks[0].rate == 99.0)
-    assert(asks[1].rate == 100.0)
-    assert(asks[2].rate == 101.0)
+    pudb.set_trace()
+    asks = m['test']['asks']
+    assert(asks[0]['rate'] == 99.0)
+    assert(asks[1]['rate'] == 100.0)
+    assert(asks[2]['rate'] == 101.0)
 
 def test_place_bids():
-    o = market.Order(
-        account = '123',
-        kind = 'bid',
-        product = 'test',
-        rate = 100.0,
-        amount = 5
-    )
+    o = {
+        'account': '123',
+        'kind': 'bid',
+        'product': 'test',
+        'rate': 100.0,
+        'amount': 5,
+    }
 
     m = {}
-    market.place_order(m, o)
-    market.place_order(m, dcs.replace(o, rate=101.0))
-    market.place_order(m, dcs.replace(o, rate=99.0))
+    rates = [99.0, 100.0, 101.0]
+    for rate in rates:
+        o = deepcopy(o)
+        o['rate'] = rate
+        market.place_order(m, **o)
 
-    bids = m['test']['bid']
-    assert(bids[2].rate == 99.0)
-    assert(bids[1].rate == 100.0)
-    assert(bids[0].rate == 101.0)
+    bids = m['test']['bids']
+    assert(bids[2]['rate'] == 99.0)
+    assert(bids[1]['rate'] == 100.0)
+    assert(bids[0]['rate'] == 101.0)
 
 def test_fill_orders():
-    bid = market.Order(
-        account = '123',
-        kind = 'bid',
-        product = 'test',
-        rate = 100.0,
-        amount = 5
-    )
+    bid = {
+        'account': '123',
+        'kind': 'bid',
+        'product': 'test',
+        'rate': 100.0,
+        'amount': 5,
+    }
 
-    ask = market.Order(
-        account = '456',
-        kind = 'ask',
-        product = 'test',
-        rate = 100.0,
-        amount = 5
-    )
+    ask = {
+        'account': '456',
+        'kind': 'ask',
+        'product': 'test',
+        'rate': 100.0,
+        'amount': 5,
+    }
 
     m = {}
-    market.place_order(m, bid)
-    market.place_order(m, ask)
     pudb.set_trace()
+    market.place_order(m, **bid)
+    market.place_order(m, **ask)
     x = 1
